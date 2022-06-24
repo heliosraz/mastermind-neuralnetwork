c12c5b5c3a02887f764bb215ad48dbcd77d4d78f
Sunny Zhou <sutzhou@ucdavis.edu>
2022-06-23 22:35:10 -0700

    filled in methods and fixed bugs

--- board.py
+++ board.py
@@ -1,5 +1,6 @@
 # Place your Board class in this file
 from typing import Union, List
+import numpy as np
 
 class MastermindBoard:
     def __init__(self, key: List[str]):
@@ -9,23 +10,56 @@ class MastermindBoard:
         self.blank = 'x'
         self.items = [[self.blank for c in range(self.cols)] for r in range(self.rows)]
         self.key= key
+        self.print_board()
 
-    def guess(self, guess: Union[str]) -> List[List[str]]:
-        for index in range(self.col-1):
-            self.items[turn][index]=guess[index]:
+    def set_row(self, item: List[str], row: int) -> List[List[str]]:
+        row=row-1
+        for index in range(self.cols):
+            self.items[row][index]=item[index]
         self.print_board()
         return self.items
 
     def print_board(self) -> None:
-        print(self.name)
-        print("  ", end="")
-        for c in range(self.rows-1):
+        print("          R/W")
+        for c in range(self.rows):
             print(str(c+1), end=" ")
-            for item in self.items[c][]:
+            for item in self.items[c]:
                 print(str(item), end=" ")
-            print(str(self.checkmatches(self.items[c][]))+"/"+str(self.checkclose(self.items[c][]))+"/n")
+            print(str(self.checkmatches(self.items[c]))+"/"+str(self.checkclose(self.items[c])))
+        print("---------------")
         return
 
-    def checkmatches(self)-> str:
+    def checkmatches(self, checking: List[str])-> int:
+        total=0
+        for index in range(self.cols):
+            if self.key[index]==checking[index]:
+                total+=1
+        return total
 
-    def checkclose(self)-> str:
+    def checkclose(self, checking: List[str])-> int:
+        total=0
+        tempkey,tempcheck=self.remove_matches(self.key, checking)
+        for index in range(len(tempcheck)):
+            if tempcheck[index]=='x':
+                continue
+            if tempcheck[index] in tempkey:
+                total+=1
+                tempkey.remove(tempcheck[index])
+        return total
+
+    def remove_matches(self, target: List[str], compare: List[str])-> List[str]:
+        output_tar=target.copy()
+        output_comp=compare.copy()
+        outputsizediff=0
+        for index in range(len(output_tar)):
+            if target[index]==compare[index]:
+                output_tar[index]='x'
+                output_comp[index]='x'
+        return output_tar, output_comp
+
+##test case
+if __name__ == '__main__':
+    board=MastermindBoard(['R','B','W','R'])
+    board.set_row(['R','B','R','B'],1)
+    board.set_row(['R','R','B','B'],2)
+    board.set_row(['R','B','R','W'],3)
